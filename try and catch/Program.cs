56640cfa5d535a2591366a1b5a0eff770f54f070
tsidevhp001 <tsidevhp001@TSI-HP-001>
2022-02-16 13:43:57 +0530

    Add project files.

---
+++ try and catch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class MyClient
+{
+    public static void Main()
+    {
+        int x = 0;
+        int div = 0;
+        try
+        {
+            div = 100 / x;
+            Console.WriteLine("This line is not executed");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Exception occured");
+        }
+        Console.WriteLine($"Result is {div}");
+    }
+}
